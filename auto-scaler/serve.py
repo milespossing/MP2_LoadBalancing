380c030629649f86ef6006d844d252c1302e6b0b
Miles Possing <miles@possing.tech>
2024-01-28 10:00:01 -0600

    Minor updates

--- auto-scaler/serve.py
+++ auto-scaler/serve.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return socket.gethostname()
+    return socket.gethostbyname(socket.gethostname())
 
 @app.route('/', methods=['POST'])
 def post():
